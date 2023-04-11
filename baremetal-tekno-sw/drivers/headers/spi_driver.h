eda8e3e037a41cdff941d9494c6c4e8d0f751882
TUTEL-TUBITAK <104363313+TUTEL-TUBITAK@users.noreply.github.com>
2023-04-11 15:58:42 +0300

    Update spi_driver.h

--- baremetal-tekno-sw/drivers/headers/spi_driver.h
+++ baremetal-tekno-sw/drivers/headers/spi_driver.h
@@ -5,8 +5,8 @@ typedef union
 		unsigned int spi_en  : 1;
 		unsigned int spi_rst : 1;
 		unsigned int cpha    : 1;
-		unsigned int cpol	 : 1;
-		unsigned int null	 : 12;
+		unsigned int cpol    : 1;
+		unsigned int null    : 12;
 		unsigned int sck_div : 16;
 	} fields;
 	uint32_t bits;
@@ -19,7 +19,7 @@ typedef union
 		unsigned int miso_full  : 1;
 		unsigned int mosi_empty : 1;
 		unsigned int miso_empty	: 1;
-		unsigned int null	    : 28;
+		unsigned int null	: 28;
 	} fields;
 	uint32_t bits;
 }spi_status;
@@ -30,7 +30,9 @@ typedef union
 		unsigned int length    : 9;
 		unsigned int cs_active : 1;
 		unsigned int direction : 1;
-		unsigned int null	   : 21;
+		unsigned int null1     : 2;
+        	unsigned int direction : 2;
+        	unsigned int null2     : 18;
 	} fields;
 	uint32_t bits;
 }spi_cmd;
